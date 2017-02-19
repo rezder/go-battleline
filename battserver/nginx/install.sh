1aa498826aedfa478964183479b19be67d9f7f07
Rene Olsen <wwwrene@runbox.com>
2017-02-19 12:58:24 +0100

    Move docker specific files out.

--- battserver/nginx/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --rm --volumes-from nginx -v $(pwd):/currdir ubuntu cp -r /currdir/battleline /var/www/html/;
-docker run --rm --volumes-from nginx -v $(pwd):/currdir ubuntu cp /currdir/battleline.conf /etc/nginx/sites-enabled;
-docker kill --signal=HUP nginx
--- battserver/nginx/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MYDIR="$(dirname "$(realpath "$0")")"
-tar -cf nginxup.tar battleline.conf install.sh
-cd html
-tar --append --file=$MYDIR/nginxup.tar battleline
-cd $MYDIR
-gzip nginxup.tar # To extract: $ tar -xzf nginxup.tar.gz
-scp  nginxup.tar.gz rho@rezder.com:/home/rho/upload/battleline/nginx/
-rm nginxup.tar.gz
