c6d3bbda38467363bfd096d439618141ed5f95fb
Rene Olsen <wwwrene@runbox.com>
2017-08-23 11:43:36 +0200

    Switch from file to database for unfinished games.

--- v2/http/games/tables.go
+++ v2/http/games/tables.go
@@ -1,18 +1,18 @@
 package games
 
 import (
-	"encoding/gob"
+	"github.com/boltdb/bolt"
 	"github.com/pkg/errors"
 	arch "github.com/rezder/go-battleline/v2/archiver/client"
+	"github.com/rezder/go-battleline/v2/db/dbhist"
 	bg "github.com/rezder/go-battleline/v2/game"
 	"github.com/rezder/go-error/log"
-	"os"
 	"strconv"
 )
 
 const (
-	//SAVEGamesFile the file where unfinished games are saved.
-	SAVEGamesFile = "server/data/savegames.gob"
+	//SAVEGamesDbFile  unfinished games hitory data base file.
+	SAVEGamesDbFile = "server/data/savegames.db"
 )
 
 //TablesServer the battleline tables server
@@ -22,7 +22,7 @@ type TablesServer struct {
 	StartGameChCl *StartGameChCl
 	pubList       *PubList
 	doneCh        chan struct{}
-	savedGames    map[string]*bg.Game
+	savedGamesDb  *dbhist.Db
 	archiver      *arch.Client
 }
 
@@ -34,7 +34,16 @@ func NewTablesServer(
 	s.pubList = pubList
 	s.StartGameChCl = NewStartGameChCl()
 	s.doneCh = make(chan struct{})
-	s.savedGames, err = loadSavedGames(SAVEGamesFile)
+	db, err := bolt.Open(SAVEGamesDbFile, 0600, nil)
+	if err != nil {
+		err = errors.Wrapf(err, "Open data base file %v failed", SAVEGamesDbFile)
+		return s, err
+	}
+	s.savedGamesDb = dbhist.New(dbhist.KeyPlayers, db, 500)
+	err = s.savedGamesDb.Init()
+	if err != nil {
+		return s, err
+	}
 	if err != nil {
 		return s, err
 	}
@@ -44,7 +53,7 @@ func NewTablesServer(
 
 //Start starts the tables server.
 func (s *TablesServer) Start(errCh chan<- error) {
-	go startTables(s.StartGameChCl, s.pubList, s.doneCh, errCh, s.savedGames, s.archiver)
+	go startTables(s.StartGameChCl, s.pubList, s.doneCh, errCh, s.savedGamesDb, s.archiver)
 }
 
 //Stop stops the tables server.
@@ -61,7 +70,7 @@ func startTables(
 	startGameChCl *StartGameChCl,
 	pubList *PubList, doneCh chan struct{},
 	errCh chan<- error,
-	savedGames map[string]*bg.Game,
+	savedGamesDb *dbhist.Db,
 	archiver *arch.Client) {
 
 	finishTableCh := make(chan *FinishTableData)
@@ -77,7 +86,12 @@ Loop:
 			delete(games, fin.ids[1])
 			if fin.game != nil {
 				if fin.game.Pos.LastMoveType.IsPause() {
-					savedGames[gameID(fin.ids)] = fin.game
+					err := savedGamesDb.Put(fin.game.Hist)
+					if err != nil {
+						errTxt := "Save game player ids: %v failed."
+						err = errors.Wrapf(err, errTxt, fin.game.Hist.PlayerIDs)
+						errCh <- err
+					}
 				} else {
 					archiver.Archive(fin.game.Hist)
 				}
@@ -93,7 +107,7 @@ Loop:
 				close(start.PlayerChs[1])
 			} else {
 				var savedGame *bg.Game
-				savedGame, start, savedGames = getOldGame(start, savedGames)
+				savedGame, start = getOldGame(start, savedGamesDb, errCh)
 				joinWatchCh := NewJoinWatchChCl()
 				go tableServe(start.PlayerIds, start.PlayerChs, joinWatchCh, savedGame, finishTableCh, errCh)
 				games[start.PlayerIds[0]] = NewGameData(start.PlayerIds[1], joinWatchCh)
@@ -110,25 +124,41 @@ Loop:
 			}
 		}
 	} //loop
-	err := saveGames(savedGames)
+	err := savedGamesDb.Close()
 	if err != nil {
 		errCh <- err
 	}
 	archiver.Stop()
 	close(doneCh)
 }
-func getOldGame(start *StartGameChData, games map[string]*bg.Game) (*bg.Game, *StartGameChData, map[string]*bg.Game) {
-	game, old := games[gameID(start.PlayerIds)]
-	if old {
-		delete(games, gameID(start.PlayerIds))
+func getOldGame(
+	start *StartGameChData,
+	hdb *dbhist.Db,
+	errCh chan<- error) (*bg.Game, *StartGameChData) {
+
+	var game *bg.Game
+	key := dbhist.KeyPlayerIDs(start.PlayerIds)
+	hist, err := hdb.Get(key)
+	if err != nil {
+		err = errors.Wrap(err, "Loading game from data base failed.")
+		errCh <- err
+	}
+	if hist != nil {
+		err = hdb.Delete(key)
+		if err != nil {
+			err = errors.Wrapf(err, "Failed deleting history for %v", start.PlayerIds)
+			errCh <- err
+		}
+		game := bg.NewGame()
+		game.LoadHist(hist)
+		_ = game.Resume() //Assumes we do not save finsihed game
 		if game.Hist.PlayerIDs != start.PlayerIds {
 			start.PlayerIds = [2]int{start.PlayerIds[1], start.PlayerIds[0]}
 			start.PlayerChs = [2]chan<- *PlayingChData{start.PlayerChs[1],
 				start.PlayerChs[0]}
 		}
-		_ = game.Resume() //Assume we do not save finsihed game
 	}
-	return game, start, games
+	return game, start
 }
 func isPlaying(ids [2]int, games map[int]*GameData) bool {
 	_, isFound := games[ids[0]]
@@ -170,58 +200,6 @@ func gameID(playerIDs [2]int) (id string) {
 type FinishTableData struct {
 	ids  [2]int
 	game *bg.Game
-}
-
-func saveGames(games map[string]*bg.Game) (err error) {
-	file, err := os.Create(SAVEGamesFile)
-	if err != nil {
-		err = errors.Wrap(err, log.ErrNo(15)+"Create games file")
-		return err
-	}
-	defer func() {
-		if cerr := file.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-	}()
-	hists := make(map[string]*bg.Hist)
-	for key, game := range games {
-		hists[key] = game.Hist
-	}
-	encoder := gob.NewEncoder(file)
-	err = encoder.Encode(hists)
-	return err
-}
-
-// loadSaveGames loads the saved games.
-func loadSavedGames(filePath string) (games map[string]*bg.Game, err error) {
-	file, err := os.Open(filePath)
-	if err == nil {
-		defer func() {
-			if cerr := file.Close(); cerr != nil && err == nil {
-				err = cerr
-			}
-		}()
-		decoder := gob.NewDecoder(file)
-		hists := make(map[string]*bg.Hist)
-		err = decoder.Decode(&hists)
-		if err == nil {
-			games = make(map[string]*bg.Game)
-			for key, hist := range hists {
-				game := bg.NewGame()
-				game.LoadHist(hist)
-				games[key] = game
-			}
-		}
-	} else {
-		if os.IsNotExist(err) {
-			err = nil
-			games = make(map[string]*bg.Game) //first start
-		} else {
-			err = errors.Wrap(err, log.ErrNo(16)+"Open saved games file")
-			return games, err
-		}
-	}
-	return games, err
 }
 
 // StartGameChData is the information need to start a game.
